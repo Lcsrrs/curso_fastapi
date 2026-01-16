96387dc2528a0d8e537daaf284c2aa34605e7673
Lucas Ramos <lcsramos.r@gmail.com>
2026-01-15 21:26:36 -0300

    aula 08 parcial

---
+++ fastapi_zero/routers/auth.py
@@ -0,0 +1,48 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi_zero.database import get_session
+from fastapi_zero.models import User
+from fastapi_zero.schemas import (
+    Token,
+)
+from fastapi_zero.security import (
+    create_access_token,
+    verify_password,
+)
+
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+Session = Annotated[AsyncSession, Depends(get_session)]
+OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+
+@router.post('/token')
+async def login_for_acess_token(
+    session: Session,
+    form_data: OAuth2Form,
+    response_model=Token,
+):
+    user = await session.scalar(
+        select(User).where(User.email == form_data.username)
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail='Incorrect email or password',
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token({'sub': user.email})
+    return {'access_token': access_token, 'token_type': 'Bearer'}
--- fastapi_zero/security.py
+++ fastapi_zero/security.py
@@ -11,13 +11,12 @@ from sqlalchemy.orm import Session
 
 from fastapi_zero.database import get_session
 from fastapi_zero.models import User
+from fastapi_zero.settings import Settings
 
-SECRET_KEY = 'alguma coisa aqui'
-ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 pwd_context = PasswordHash.recommended()
+settings = Settings()
 
 
 def get_password_hash(password: str):
@@ -26,19 +25,6 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-
-    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
-    )
-    to_encode.update({'exp': expire})
-
-    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-    return encoded_jwt
 
 
 def get_current_user(
@@ -52,7 +38,9 @@ def get_current_user(
     )
 
     try:
-        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
         subject_email = payload.get('sub')
 
         if not subject_email:
@@ -67,3 +55,18 @@ def get_current_user(
         raise credential_exception
 
     return user
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({'exp': expire})
+
+    encoded_jwt = encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
